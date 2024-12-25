77ab94e494aea18ff8be504960b44ee75dd39e6f
tanavposwal <tanavposwal939@gmail.com>
2024-12-25 15:58:14 +0530

    add bottom tab

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,7 +1,11 @@
+import { DownloadPicture } from "@/components/BottomSheet";
 import { router } from "expo-router";
+import { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Account() {
+  const [picture, setPicture] = useState(false);
+
   return (
     <View
       style={{
@@ -12,6 +16,8 @@ export default function Account() {
     >
       <Text>Account</Text>
       <Button title="Go to Account Info" onPress={() => router.push("/accountinfo")} />
+      <Button title="Dwonload Picture" onPress={() => setPicture(true)} />
+      {picture && <DownloadPicture />}
     </View>
   );
 }
