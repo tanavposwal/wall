c0a1003ab732f77bf57551f0b3dc90506f52e726
tanavposwal <tanavposwal939@gmail.com>
2024-12-25 12:43:55 +0530

    add top tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,14 @@
 import { Text, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function ForYou() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>4 u</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Suggested" component={View} />
+      <Tab.Screen name="Liked" component={View} />
+      <Tab.Screen name="Library" component={View} />
+    </Tab.Navigator>
   );
 }
